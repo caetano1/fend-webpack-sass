e314c93a63e536977f69d7590b68bf354b142067
caetano <joaoluisfcaetano@gmail.com>
2020-10-23 15:02:19 -0300

    Performing the api call challenge

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,14 +6,28 @@ function handleSubmit(event) {
 
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
+    // Api challenge
+    const zipCode = 10001;
+    const apiKey = '781d27be53ccaae278c356ef46876e8a';
+    const uri = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`;
+
+    fetch(uri)
+    .then(res => {
+        return res.json()
+    })
+    .then(function(data) {
+        /* console.log(data) */
+        document.getElementById('results').innerHTML = `City name is ${data.name}`
+    })
+
+    /* console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => {
         return res.json()
     })
     .then(function(data) {
         document.getElementById('results').innerHTML = data.message
-    })
+    }) */
 }
 
 export { handleSubmit }
